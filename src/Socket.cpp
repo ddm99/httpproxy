5b084f19b8a8f447b0eb436e57ee3596f49b0d64
ddm99 <zcapndi@gmail.com>
2022-02-13 16:35:29 -0500

    update test example

--- src/Socket.cpp
+++ src/Socket.cpp
@@ -1,11 +1,12 @@
 #include "Socket.hpp"
 
 int main() {
+  Socket server(NULL, "4444");
+  server.serverSocket();
+  std::vector<char> Vector = server.receiveFromClient();
   Socket client("localhost", "4444");
   std::string newString = "edit this"; 
   std::vector<char> newVector(newString.begin(), newString.end()); 
   client.sendtoServer(newVector);
-  //s1.serverSocket();
-  //std::vector<char> newVector = s1.receiveFromClient();
   return 0;
 }
