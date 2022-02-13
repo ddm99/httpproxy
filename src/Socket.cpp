da3ea7ab365080a281d9d8bbb34e51790b41a5a1
sj346 <sj346@duke.edu>
2022-02-13 15:26:07 -0500

    makeSocket() works. example.com, 3490

---
+++ src/Socket.cpp
@@ -0,0 +1,6 @@
+#include "Socket.hpp"
+
+int main() {
+  Socket s1("www.example.com", "3490");
+  return 0;
+}
--- src/Socket.hpp
+++ src/Socket.hpp
@@ -1,19 +1,52 @@
-#include <iostream>
-#include <cstring>
-#include <sys/socket.h>
 #include <netdb.h>
+#include <sys/socket.h>
 #include <unistd.h>
-#include <string>
-#include <cstring>
 
-using namespace std;
-class Socket{
+#include <cstring>
+#include <iostream>
+#include <string>
+
+class Socket {
   int status;
   int socket_fd;
   struct addrinfo host_info;
-  struct addrinfo *host_info_list;
-  string hostname;
-  string port;
+  struct addrinfo * host_info_list;
+  std::string hostname;
+  std::string port;
 
+ public:
+  //Basic constructor for creating a socket with the specified hostname and port
+  Socket(std::string anotherHostName, std::string anotherPort) {
+    hostname = anotherHostName;
+    port = anotherPort;
+    makeSocket();
+  }
 
+  //Function responsible for making a socket
+  void makeSocket() {
+    memset(&host_info, 0, sizeof(host_info));
+
+    host_info.ai_family = AF_UNSPEC;
+    host_info.ai_socktype = SOCK_STREAM;
+    host_info.ai_flags = AI_PASSIVE;
+
+    status = getaddrinfo(hostname.c_str(), port.c_str(), &host_info, &host_info_list);
+
+    if (status != 0) {
+      std::cerr << "Error: cannot get address info for host" << std::endl;
+      std::cerr << "  (" << hostname << "," << port << ")" << std::endl;
+      // return -1;
+    }  //if
+
+    socket_fd = socket(host_info_list->ai_family,
+                       host_info_list->ai_socktype,
+                       host_info_list->ai_protocol);
+    if (socket_fd == -1) {
+      std::cerr << "Error: cannot create socket" << std::endl;
+      std::cerr << "  (" << hostname << "," << port << ")" << std::endl;
+      // return -1;
+    }  //if
+
+    std::cout << status << "\n";
+  }
 };
