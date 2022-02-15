977fd722f621e3362eb7fcb9c8b7b340a6cc8571
ddm99 <zcapndi@gmail.com>
2022-02-15 18:21:04 -0500

    read file into string

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,25 +1,27 @@
 #include "Parser.hpp"
+#include <iostream>
 #include <fstream>
-#include <cassert>
-#include <iterator>
+#include <sstream>
 
-void ReadFromFile(std::vector<char> &x, const std::string &file_name)
-{
-    std::ifstream read_file(file_name);
-    assert(read_file.is_open());
+using std::cout; using std::cerr;
+using std::endl; using std::string;
+using std::ifstream; using std::ostringstream;
 
-    std::copy(std::istream_iterator<char>(read_file), std::istream_iterator<char>(),
-        std::back_inserter(x));
-
-    read_file.close();
+string readFileIntoString(const string& path) {
+    ifstream input_file(path.c_str());
+    if (!input_file.is_open()) {
+        cerr << "Could not open the file - '"
+             << path << "'" << endl;
+    }
+    return string((std::istreambuf_iterator<char>(input_file)), std::istreambuf_iterator<char>());
 }
 
 int main()
 {
-    std::vector<char> req;
-    ReadFromFile(req, "req.txt");
-    for (size_t i = 0; i < req.size(); i++) {
-      std::cout << req[i];
-    }
-    std::cout << std::endl;
+    string filename("req.txt");
+    string file_contents;
+
+    file_contents = readFileIntoString(filename);
+    cout << file_contents << endl;
+
 }
